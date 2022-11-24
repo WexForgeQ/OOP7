be5646e19ce94e1160b13baa835543b5e325adf7
WexForgeQ <95540615+WexForgeQ@users.noreply.github.com>
2022-11-24 16:23:57 +0300

    Исключение. Пользовательский класс(тип))
    Добавлена проверка на тип bool и исключение к ней. Так же реализована коллекция с пользовательским типом Question.

--- LAB7/Program.cs
+++ LAB7/Program.cs
@@ -12,7 +12,7 @@ namespace LAB7
     }
     class CollectionType<T>: ICommon<T>
     {
-        Set<T> A = new Set<T>();
+        public Set<T> A = new Set<T>();
         public void Add(T item)
         {
             A.AddItemSet(item);
@@ -35,18 +35,22 @@ namespace LAB7
 
         public class Set<T>
         {
-
             public List<T> members = new List<T>();
             public static int countofsets = 0;
             public int number = 0;
             public Set(List<T> items)
             {
+                if (members is List<bool>)
+                    throw new Exception("Данный тип недопустим в множестве");
+
                 members = items;
                 countofsets++;
                 number = countofsets;
             }
             public Set()
             {
+                if(members is List<bool>)
+                    throw new Exception("Данный тип недопустим в множестве");
                 countofsets++;
                 number = countofsets;
             }
@@ -116,10 +120,49 @@ namespace LAB7
             }
         }
     }
-   
+    public class Question
+    {
+        public string question;
 
- class Program
+        public string Quest
+        {
+            get
+            {
+                return question;
+            }
+            set
+            {
+                if (value == null)
+                    throw new Exception("Вопрос не может быть пустым");
+                else if (!value.Contains('?'))
+                    throw new Exception("В вопросе отсутствует символ <?>");
+                else
+                    question = value;
+            }
+        }
+
+        public Question()
+        {
+            question = "Default";
+        }
+
+        public Question(string que)
+        {
+            question = que;
+        }
+        public void DisplayQuestion()
+        {
+            Console.WriteLine("Вопрос: " + question);
+        }
+        public override string ToString()
+        {
+            return "Тип объекта:" + GetType() + " Вопрос:" + question;
+        }
+    }
+
+    class Program
  {
+
         static void Main(string[] args)
         {
             CollectionType<int> A = new CollectionType<int>();
@@ -128,10 +171,23 @@ namespace LAB7
             A.LookThrough();
             A.Delete(1);
             A.LookThrough();
-
-
-
-
+            try
+            {
+                CollectionType<bool> B = new CollectionType<bool>();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Проверка закончена");
+            }
+            CollectionType<string> C = new CollectionType<string>();
+            Question Q = new Question();
+            CollectionType<Question> Qtype = new CollectionType<Question>();
+            Qtype.Add(Q);
+            Qtype.LookThrough();
         }
 
  }
